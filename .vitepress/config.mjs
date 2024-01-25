52e1c364cb042ff089589672b719c2ab203b0388
Your Name <you@example.com>
2024-01-26 02:00:36 +0800

    first commit

---
+++ .vitepress/config.mjs
@@ -0,0 +1,62 @@
+import { defineConfig } from 'vitepress'
+import { set_sidebar } from "./utils/auto-sidebar.mjs"
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "文明之间の交流",
+  description: "这是一个不明所以的主页",
+  themeConfig: {
+    outlineTitle: "目录",
+    outline: [2,6],
+    sidebar: false, // 关闭侧边栏
+    aside: "left", // 设置右侧侧边栏在左侧显示
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: '主页', items:[
+        {text:'首页',link:'/'},
+        {text:'云崽环境部署',link:'/markdown-examples'},
+        {text:'部署云崽',link:'/markdown-examples'},
+      ] },
+      { text: '文档', link: '/api-examples' }
+    ],
+
+      // sidebar: [
+      //   {
+      //     text: '文档',
+      //     items: [
+      //       { text: '云崽所需环境', link: '/markdown-examples' },
+      //       { text: '部署云崽', link: '/api-examples' }
+      //     ]
+      //   }
+      // ],
+    sidebar: { 
+      "/front-end/react": set_sidebar("front-end/react") 
+    },
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+    ],
+    footer:{
+      copyright:"Copyright@ 2023 Hekina"
+    },
+       // 设置搜索框的样式
+       search: {
+        provider: "local",
+        options: {
+          translations: {
+            button: {
+              buttonText: "搜索文档",
+              buttonAriaLabel: "搜索文档",
+            },
+            modal: {
+              noResultsText: "无法找到相关结果",
+              resetButtonTitle: "清除查询条件",
+              footer: {
+                selectText: "选择",
+                navigateText: "切换",
+              },
+            },
+          },
+        },
+      },
+  }
+})
---
+++ index.md
@@ -0,0 +1,25 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "文明之间の交流"
+  text: "这是莫名其妙的主页"
+  tagline: 这里放一些神秘代码
+  actions:
+    - theme: alt
+      text: 这啥
+      link: /
+    - theme: brand
+      text: Play
+      link: /api-examples
+
+features:
+  - title: 云崽所需环境
+    details: 云崽机器人安装一些所需的环境
+  - title: 搭建云崽
+    details: 搭建完环境之后，一步一步的安装云崽
+  - title: 云崽机器人插件
+    details: 根据喜好安装云崽的一些插件
+---
+
